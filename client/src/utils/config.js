4aead330bf3bf2830326cad52e9360aae50207c5
Aravind663 <aravindpentaparthi@gmail.com>
2026-01-07 23:27:31 +0530

    Deployed backend on render

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,6 +1,6 @@
 export const getBaseUrl = () => {
   if (process.env.NODE_ENV === "production") {
-    return "https://planx-backend.onrender.com/api/v1";
+    return "https://planx-backend.onrender.com";
   }
   return "http://localhost:5000/api/v1";
 };
